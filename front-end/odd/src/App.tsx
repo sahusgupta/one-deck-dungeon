64c1ff07937c281bed2488019863bb8f7b65fab1
Daniel <danielrupawalla@gmail.com>
2024-08-29 22:46:06 -0500

    bored

--- front-end/odd/src/App.tsx
+++ front-end/odd/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import HomePage from "./pages/home";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
