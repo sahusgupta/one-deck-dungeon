35010b8694c179045af1580985e39ba2095ee56d
Pratyush Prakash <geekrocks42@gmail.com>
2024-09-03 09:14:05 -0500

    added floor, changed game

---
+++ front-end/odd/src/middle-end/Floor/Floor.tsx
@@ -0,0 +1,5 @@
+class Floor {
+    constructor(parameters) {
+        
+    }
+}
--- front-end/odd/src/middle-end/RuntimeFiles/Game.tsx
+++ front-end/odd/src/middle-end/RuntimeFiles/Game.tsx
@@ -1,17 +1,48 @@
 class Game {
     private playerList: Array<Player>;
     private dungeon: Dungeon;
+    public gameId: string;
+    private potions: number;
   
-    public constructor() {
+    public constructor(gameId: string) {
         this.playerList = new Array<Player>();
-      console.log("Hello world");
+        this.dungeon = new Dungeon();
+        this.gameId = gameId;
+        this.potions = 0;
+        console.log("Hello world");
     }
     
-    public getPlayers() : Array<Player> {
+    public get getPlayerList() : Array<Player> {
         return this.playerList;
     }
     
-  }
+    public set setPlayerList(v : Array<Player>) {
+        this.playerList = v;
+    }
+    
+    public get getDungeon() : Dungeon {
+        return this.dungeon;
+    }
+  
+    public set setDungeon(v : Dungeon) {
+        this.dungeon = v;
+    }
+    
+    
+    public get getPotions() : number {
+        return this.potions;
+    }
+
+    
+    public set setPotions(v : number) {
+        this.potions = v;
+    }
+
+    public incrementPotions() : void {
+        this.potions++;
+    }
+    
+}
   
 //   const person = new Person("Jane");
 //   console.log(person.getName()); // person.name isn't accessible from outside the class since it's private
