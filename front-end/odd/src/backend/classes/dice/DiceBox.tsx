6110c747a3737371f06c7662a3e5d0891350d160
Sahus Gupta <63523632+sahusgupta@users.noreply.github.com>
2024-09-04 09:39:24 -0500

    changed naming conventions

--- front-end/odd/src/backend/classes/dice/DiceBox.tsx
+++ front-end/odd/src/backend/classes/dice/DiceBox.tsx
@@ -1,30 +1,30 @@
 import { Dice } from "./Dice";
 
 class DiceBox {
-    neededRoll: number;
-    constrainedToOne: boolean;
-    requiredToFill: boolean;
-    heldDice: number
+    _neededRoll: number;
+    _constrainedToOne: boolean;
+    _requiredToFill: boolean;
+    _heldDice: number
 
     constructor(need: number, constrained: boolean, required: boolean){
-        this.neededRoll = need;
-        this.constrainedToOne = constrained;
-        this.requiredToFill = required;
-        this.heldDice = 0;
+        this._neededRoll = need;
+        this._constrainedToOne = constrained;
+        this._requiredToFill = required;
+        this._heldDice = 0;
     }
 
-    getNeededRoll(): number{return this.neededRoll;}
-    getConstrainedToOne(): boolean{return this.constrainedToOne}
-    getRequiredToFill(): boolean{return this.requiredToFill}
-    getheldDice(): number{return this.heldDice;}
-    setNeededRoll(n: number): void {this.neededRoll = n}
-    setConstrainedToOne(b: boolean): void {this.constrainedToOne = b}
-    setRequiredToFill(b: boolean): void {this.requiredToFill = b}
-    setHeldDice(n: number): void {this.heldDice = n}
+    getNeededRoll(): number{return this._neededRoll;}
+    getConstrainedToOne(): boolean{return this._constrainedToOne}
+    getRequiredToFill(): boolean{return this._requiredToFill}
+    getheldDice(): number{return this._heldDice;}
+    setNeededRoll(n: number): void {this._neededRoll = n}
+    setConstrainedToOne(b: boolean): void {this._constrainedToOne = b}
+    setRequiredToFill(b: boolean): void {this._requiredToFill = b}
+    setHeldDice(n: number): void {this._heldDice = n}
 
     addDice(dice: Dice){
-        this.neededRoll -= dice.value;
-        this.heldDice += 1;
+        this._neededRoll -= dice.value;
+        this._heldDice += 1;
     }
 }
 
