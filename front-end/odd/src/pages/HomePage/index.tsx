a65115aee980407480e922ad2aed208ce0b45740
Sahus Gupta <63523632+sahusgupta@users.noreply.github.com>
2024-09-10 09:17:10 -0500

    here

--- front-end/odd/src/pages/HomePage/index.tsx
+++ front-end/odd/src/pages/HomePage/index.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { HiChevronDoubleRight } from "react-icons/hi";
 import { useNavigate } from 'react-router-dom';
+import { db } from '../../backend/firebase/firebase_utils';
 
 const HomePage: React.FC = () => {
   const userName = "Daniel Rupawalla" //pull from db
   const navigate = useNavigate();
   const reRoute = (playerCount: string) => {
-    localStorage.setItem('playerCount', playerCount)
-    if(!localStorage.getItem('characterSelected')){
-      navigate("/select-char")
-    }
-    else{
-      const url = "/dungeon-select";
-      navigate(url)
-    }
-};
+    console.log(playerCount);
+    const url = "/" + playerCount;
+    navigate(url)
+  };
   return (
     <div
       className="relative w-full h-screen bg-cover bg-center"
@@ -49,7 +45,7 @@ const HomePage: React.FC = () => {
           </button>
 
           {/* 2 Player Button */}
-          <button className="w-56 h-56 bg-black bg-opacity-70 flex flex-col items-center justify-center px-6 py-4 rounded-lg space-y-2 hover:bg-opacity-80" onClick={() => reRoute("two-player")}>
+          <button className="w-56 h-56 bg-black bg-opacity-70 flex flex-col items-center justify-center px-6 py-4 rounded-lg space-y-2 hover:bg-opacity-80">
             <span className="text-xl font-bold">2 Player</span>
             <div className="w-24 h-24 rounded-full bg-gradient-to-r from-orange-400 to-yellow-500 flex items-center justify-center">
               <svg
